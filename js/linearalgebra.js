d2395ca3de1504c509bdc0ac64776c779f5923ba
sevenblackthorns <161628029+sevenblackthorns@users.noreply.github.com>
2024-07-11 17:19:06 +0200

    Update linearalgebra.js

--- js/linearalgebra.js
+++ js/linearalgebra.js
@@ -1,3 +1,22 @@
 class Matrix {
+    constructor(Data) {
+        this.Data = Data;
+        this.Shape = ArrayShape(Data);
+    }
+}
+
+function ArrayRank(ArrayA) {
+    if (!Array.isArray(ArrayA)) {
+        return 0;
+    }
+    if (ArrayA.length > 0) {
+        return ArrayRank(ArrayA[0]) + 1
+    }
+    else {
+        return 0;
+    }
+}
+
+function ArrayShape(ArrayA) {
     
 }
